9c9551e0a6af644e0f74da4d5f6e1f886fd304c8
Democles85 <sixheitartari12@gmail.com>
2021-10-19 17:35:19 +0200

    Rechecked files

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -19,7 +19,11 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Sixhei Tartari" />
         <meta name="author" content="sixheitartari" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="images/favicon.ico"
+          type="image/x-icon"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@LeJhin1" />
         <meta name="twitter:creator" content="@LeJhin1" />
